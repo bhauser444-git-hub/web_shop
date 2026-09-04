1a85f33a82ca3936627f2b3326a580622abff9ad
Terumi Hauser <terumi.hauser@gmx.de>
2026-09-04 18:19:11 +0200

    feat: connection to sql database established, html file with table that shows product data from database

--- web_server.js
+++ web_server.js
@@ -1,14 +1,33 @@
 
-// import express from 'express'
-// import {sagHallo} from './rechner.js'
+import express from 'express';
+import mysql from "mysql2";
+//import {sagHallo} from './rechner.js'
 
-// const app = express();
-// const PORT = 3000;
+const app = express();
+const PORT = 5000; 
+const sql_connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Ika7musumE!",
+    database: "web_shop"
+});
 
-// // Die Startseite
-// app.get('/', (req, res) => {
-//     res.send('Hallo von der Express-Startseite!');
-// });
+sql_connection.connect((err) => {
+    if (err) {
+        console.error("Connection failed:", err);
+        return;
+    }
+    console.log("Connection to SQL database established.");
+});
+
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.send(`
+        <h1>Web Shop</h1>
+        <a href="/products">Go to products</a>
+    `);
+});
 
 // // Eine weitere Unterseite (super einfach mit Express!)
 // app.get('/ueber-uns', (req, res) => {
@@ -17,25 +36,29 @@
 //     //res.send('Das ist die Über-Uns-Seite.');
 // });
 
-// app.listen(PORT, () => {
-//     console.log(`Express-Server läuft auf http://localhost:${PORT}`);
-// });
-import express from 'express';
-import {sagHallo} from './rechner.js'
-const app = express();
-const PORT = 5000; // Neuer Port!
+app.get('/api/products', (req, res) => {
+    sql_connection.execute(
+        "SELECT * FROM products",
+        (error, results) => {
+            if (error) {
+                console.error(error);
 
-app.get('/', (req, res) => {
-    res.send('Der Express-Server funktioniert im Minimal-Test!');
+                res.status(500).json({
+                    error: "Data base error"
+                });
+
+                return;
+            }
+
+            res.json(results);
+        }
+    );
 });
 
-// Eine weitere Unterseite (super einfach mit Express!)
-app.get('/ueber-uns', (req, res) => {
-    const gruss = sagHallo('Entwickler');
-    res.send(gruss);
-    //res.send('Das ist die Über-Uns-Seite.');
+app.get('/products', (req, res) => {
+    res.sendFile('products.html', { root: 'public' });
 });
 
 app.listen(PORT, () => {
-    console.log(`Minimal-Server läuft auf http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
